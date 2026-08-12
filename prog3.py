4c72d0405a860c854f3d1819e1628d2cfff6bce3
Mathis <Matt.kiq@icloud.com>
2026-08-12 12:33:23 +0200

    premier jour

---
+++ prog3.py
@@ -0,0 +1,5 @@
+nourriture_preferee= "pizza"
+nouvelle_nourriture_preferee = "sushi" "
+nourriture_preferee = pates
+
+print(f"Bonjour,  ma nourriture preferee est {nourriture_preferee}.")
---
+++ prog4.py
@@ -0,0 +1,12 @@
+# ...existing code...
+nom = "k"
+prenom = "mathis"
+age = 13
+taille = 1.72
+est_etudiant = False
+
+print(f"Nom: {nom}")
+print(f"Prénom: {prenom}")
+print(f"Âge: {age}")
+print(f"Taille: {taille} m")
+print(f"Étudiant: {est_etudiant}")
